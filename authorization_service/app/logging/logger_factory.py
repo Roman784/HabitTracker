1fab6c4f2f79001857d1b640c1b083b4a9ed89e2
Roman784 <rmironenko50@gmail.com>
2024-12-14 18:41:36 +0300

    Logger refactoring

--- authorization_service/app/logging/logger_factory.py
+++ authorization_service/app/logging/logger_factory.py
@@ -11,18 +11,32 @@ DATABASE_FILE_NAME = getenv('DATABASE_LOGS_FILE')
 REPOSITORY_FILE_NAME = getenv('REPOSITORY_LOGS_FILE')
 
 
+loggers = {}
+
+
 def get_database_logger() -> logging.Logger:
     '''Возвращает логер для базы данных'''
-    return get_logger('database', DATABASE_FILE_NAME)
-
+    return create_logger('database', DATABASE_FILE_NAME)
 
 def get_user_repository_logger() -> logging.Logger:
-    '''Возвращает логер для репозитория пользователей.'''
+    '''Возвращает логер для репозитория пользователей'''
     return get_logger('user_repository', REPOSITORY_FILE_NAME)
 
 
 def get_logger(module_name, file_name) -> logging.Logger:
-    '''Возвращает логер'''
+    '''
+    Возвращает логер по имени файла
+    Или создаёт новый, если такого нет
+    '''
+    if file_name in loggers:
+        return loggers[file_name]
+    
+    logger = create_logger(module_name, file_name)
+    loggers[file_name] = logger
+    return logger
+
+def create_logger(module_name, file_name) -> logging.Logger:
+    '''Создаёт логер'''
     logger = logging.getLogger(module_name)
     logger.setLevel(logging.DEBUG)
 
