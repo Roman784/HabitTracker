657f9e94b6a7fbfe635c54fd04467901393680ee
Roman784 <rmironenko50@gmail.com>
2024-12-16 14:28:39 +0300

    Delete old auth service

--- authorization_service/app/storage/models/base_model.py
+++
@@ -1,9 +0,0 @@
-'''Базовая модель'''
-
-
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
-
-class BaseModel(DeclarativeBase):
-    '''База для всех таблиц'''
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, unique=True)
